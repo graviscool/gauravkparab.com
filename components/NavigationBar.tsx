952ef600958dedc384283662abbdd7f3e97707c7
graviscool <88552913+graviscool@users.noreply.github.com>
2024-06-01 10:50:16 -0700

    feature(navbar): enhance some things

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
-import { Button } from "react-bootstrap";
+import { Button, Image } from "react-bootstrap";
 
 export default function NavigationBar() {
   return (
@@ -13,8 +13,8 @@ export default function NavigationBar() {
         <Nav className="me-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#prevexp">Previous Experience</Nav.Link>
           <Nav.Link href="#projects">Projects</Nav.Link>
-          <Nav.Link href="#pastexp">Past Experience</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
         </Nav>
         <Nav>
@@ -22,17 +22,18 @@ export default function NavigationBar() {
             Download Resume
           </Nav.Link>
         </Nav>
+
         <Navbar.Brand href="/linkedin">
-          <img
+          <Image
             src="/images/linkedin-logo.png"
-            width="30"
-            height="30"
+            width={30}
+            height={30}
             className="d-inline-block align-top"
             alt="Linkedin"
           />
         </Navbar.Brand>
         <Navbar.Brand href="/github">
-          <img
+          <Image
             src="/images/github-logo-white.png"
             width="30"
             height="30"
@@ -50,7 +51,7 @@ export default function NavigationBar() {
         </Form>
         <Button
           variant="outline-info"
-          onClick={(e) => alert(`This feature hasn't been implemented yet`)}
+          onClick={(e) => alert(`Searching hasn't been implemented yet`)}
         >
           Search
         </Button>
