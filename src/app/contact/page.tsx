ea01daeae8eac1a2dbb92e0b8bde177ff9ac9d14
graviscool <88552913+graviscool@users.noreply.github.com>
2024-06-29 15:32:05 +0530

    feat(contact): impl email functionality (now need to do alert)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,68 +12,92 @@ import {
 import ContactNavbar from "../components/ContactNavbar";
 import styles from "@/styles/Contact.module.css";
 import ContactFooter from "../components/ContactFooter";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
   return (
     <>
-      <Container fluid className="bg-secondary text-light">
+      <Container fluid className="bg-dark text-info">
         <ContactNavbar />
-        <p className={`mt-3 mb-2 fs-2 ms-2 ${styles.contactText}`}>
-          If you have any questions or would just like to reach out, feel free
-          to reach out to me at my email{" "}
-          <a href="mailto:gkparab1@gmail.com" className={styles.emailLink}>
-            gkparab1@gmail.com
-          </a>
-          . You can also contact me on{" "}
-          <a
-            href="https://www.linkedin.com/in/gparab"
-            target="_blank"
-            rel="noreferrer noopener"
-            className={styles.linkedinLink}
-          >
-            Linkedin
-          </a>
-          . I look forward to hearing from you!
-        </p>
-        <br />
-        <p>OR, you can fill out the contact form below:</p>
-        <div className="bg-secondary vh-100">
-          <Form onSubmit={sendContactEmail}>
-            <Row className="g-2">
-              <Col>
-                <FormGroup controlId="formGroupText">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control required name="name"></Form.Control>
-                </FormGroup>
-              </Col>
-              <Col>
-                <Form.Group className="mb-3" controlId="formGroupEmail">
-                  <Form.Label>Email Address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    name="email"
-                    placeholder="name@example.com"
-                    required
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Form.Group className="mb-3" controlId="formGroupTextarea">
-              <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" rows={3} required name="message" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </div>
+        <Row>
+          <Col>
+            <h1 className={`mt-4 ${styles.connectText}`}>
+              Let&apos;s Connect!
+            </h1>
+            <div className="text-light">
+              <p className={`mt-3 mb-2 fs-2 ms-2 ${styles.contactText}`}>
+                Email
+                <br />
+                <a href="mailto:gkparab1@gmail.com" className="text-light">
+                  gkparab1@gmail.com
+                </a>
+                <br />
+                <br />
+                Linkedin
+                <br />
+                <a
+                  href="https://www.linkedin.com/in/gparab"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="text-light"
+                >
+                  https://linkedin.com/in/gparab
+                </a>
+              </p>
+            </div>
+          </Col>
+          <Col>
+            <Form
+              onSubmit={sendContactEmail}
+              className="me-2 mt-3 pt-5 text-success"
+            >
+              <Row className="g-2">
+                <Col>
+                  <FormGroup controlId="formGroupText">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      required
+                      name="name"
+                      className={styles.formField}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col>
+                  <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      required
+                      className={styles.formField}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Form.Group className="mb-3" controlId="formGroupTextarea">
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={2}
+                  required
+                  name="message"
+                  className={styles.formField}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <div className="bg-dark vh-100"></div>
       </Container>
       <ContactFooter />
     </>
   );
 }
 
-const sendContactEmail = (event: React.FormEvent<HTMLFormElement>) => {
+const sendContactEmail = async (event: React.FormEvent<HTMLFormElement>) => {
   event.preventDefault();
 
   const formD = new FormData(event.currentTarget);
@@ -82,5 +106,12 @@ const sendContactEmail = (event: React.FormEvent<HTMLFormElement>) => {
   const email = formD.get("email") as string;
   const message = formD.get("message") as string;
 
-  alert(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  await emailjs.send(
+    "contact_send",
+    "template_contact",
+    { name: name, email: email, message: message },
+    {
+      publicKey: process.env.EMAILJS_PUBLIC_KEY,
+    }
+  );
 };
