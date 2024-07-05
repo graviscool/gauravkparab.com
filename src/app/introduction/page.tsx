9378e12f8c3743772c63f4cafd0d0f2d76d92192
graviscool <88552913+graviscool@users.noreply.github.com>
2024-07-05 15:09:50 +0530

    add some animations using framer-motion :)

--- src/app/introduction/page.tsx
+++ src/app/introduction/page.tsx
@@ -3,17 +3,53 @@ import { motion, useScroll, useSpring } from "framer-motion";
 import Container from "react-bootstrap/Container";
 import styles from "@/styles/Introduction.module.css";
 import IntroNavbar from "../components/IntroNavbar";
+import { useRef } from "react";
 
 export default function Introduction() {
+  const { scrollYProgress: totalScrollProgress } = useScroll();
+
+  const namephotoSection = useRef(null);
+  const { scrollYProgress: namephotoScrollYProgress } = useScroll({
+    target: namephotoSection,
+    offset: ["start", "end 0.20"],
+  });
+
+  const locationSection = useRef(null);
+  const { scrollYProgress: locationScrollYProgress } = useScroll({
+    target: locationSection,
+    offset: ["start", "end 0.20"],
+  });
+
+  const educationSection = useRef(null);
+  const { scrollYProgress: educationScrollYProgress } = useScroll({
+    target: educationSection,
+    offset: ["start", "end 0.20"],
+  });
+
   return (
     <>
+      <motion.div
+        className={styles.scrollProgressTop}
+        style={{ scaleX: totalScrollProgress }}
+      />
       <IntroNavbar />
       <Container fluid className={styles.introContainer}>
         <section id="namephoto">
-          <div className="vh-100 bg-info"></div>
+          <div
+            className="vh-100 bg-dark d-flex flex-column"
+            ref={namephotoSection}
+          >
+            <motion.div
+              className={`${styles.scrollProgressSection} mt-auto`}
+              style={{ scaleX: namephotoScrollYProgress }}
+            />
+          </div>
         </section>
         <section id="location">
-          <div className="vh-100 overflow-hidden position-relative">
+          <div
+            className="vh-100 overflow-hidden position-relative d-flex flex-column"
+            ref={locationSection}
+          >
             <video autoPlay muted loop className={styles.backgroundVideo}>
               <source src="videos/sf_video_v2.mp4" type="video/mp4" />
             </video>
@@ -26,13 +62,23 @@ export default function Introduction() {
             >
               Based in the San Francisco Bay Area
             </motion.h1>
+            <motion.div
+              className={`${styles.scrollProgressSection} mt-auto`}
+              style={{ scaleX: locationScrollYProgress }}
+            />
           </div>
         </section>
         <section id="education">
-          <div className="vh-100 bg-secondary"></div>
+          <div className="vh-100 bg-secondary d-flex flex-column" ref={educationSection}>
+            <motion.div
+              className={`${styles.scrollProgressSection} mt-auto`}
+              style={{ scaleX: educationScrollYProgress }}
+            />
+          </div>
         </section>
         <section id="hobbies">
-          <div className="vh-100 bg-primary"></div>
+          <div className="vh-100 bg-primary">
+          </div>
         </section>
       </Container>
     </>
