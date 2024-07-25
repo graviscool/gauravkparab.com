d7f42898336447968bba5629c0e8258184e60490
graviscool <88552913+graviscool@users.noreply.github.com>
2024-07-24 20:32:05 -0700

    add some more anims + some other stuff!

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,13 +1,13 @@
 import React, { useRef, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
-import CardGroup from "react-bootstrap/CardGroup";
 import { Row, Col, Toast, ToastContainer, Button } from "react-bootstrap";
 import styles from "@/styles/Main.module.css";
 import { motion, useScroll } from "framer-motion";
 import TypeIt, { TypeItProps } from "typeit-react";
 import { ParallaxBanner } from "react-scroll-parallax";
 import Head from "next/head";
+import TechStack from "./TechStack";
 
 export default function Main() {
   const [showMobileToast, setShowMobileToast] = useState(true);
@@ -223,7 +223,12 @@ export default function Main() {
                   </Card>
                 </Row>
               </section>
-              <div className={styles.topLang}>
+              <motion.div
+                className={styles.topLang}
+                initial={{ translateX: "-100%" }}
+                whileInView={{ translateX: 0 }}
+                viewport={{ once: true }}
+              >
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <picture>
                   <source
@@ -249,7 +254,7 @@ export default function Main() {
                     alt="Top Languages Used"
                   />
                 </picture>
-              </div>
+              </motion.div>
               <section id="prevexp">
                 <div className="d-flex">
                   <h2 className={styles.headingTwo}>Work Experience</h2>
@@ -306,119 +311,7 @@ export default function Main() {
                     top
                   </a>
                 </div>
-                <CardGroup className="text-center mx-3">
-                  {/* <Row className="text-center"> */}
-                  <Card
-                    className={`${styles.stackCard} mx-md-2 my-sm-3`}
-                    bg="info"
-                    text="dark"
-                    border="success"
-                  >
-                    <Card.Img variant="top" src="/images/langs/python.png" />
-                    <Card.Body>
-                      <Card.Text>Python</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  <Card
-                    className={`${styles.stackCard} mx-md-2 my-sm-3`}
-                    bg="info"
-                    text="dark"
-                    border="success"
-                  >
-                    <Card.Img
-                      variant="top"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg"
-                    />
-                    <Card.Body>
-                      <Card.Text>Java</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  <Card
-                    className={`${styles.stackCard} mx-md-2 my-sm-3`}
-                    bg="info"
-                    text="dark"
-                    border="success"
-                  >
-                    <Card.Img
-                      variant="top"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-                    />
-                    <Card.Body>
-                      <Card.Text>Javascript</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  <Card
-                    className={`${styles.stackCard} mx-md-2 my-sm-3`}
-                    bg="info"
-                    text="dark"
-                    border="success"
-                  >
-                    <Card.Img
-                      variant="top"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-                    />
-                    <Card.Body>
-                      <Card.Text>React</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  <Card
-                    className={`${styles.stackCard} mx-md-2 my-sm-3`}
-                    bg="info"
-                    text="dark"
-                    border="success"
-                  >
-                    <Card.Img
-                      variant="top"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"
-                    />
-                    <Card.Body>
-                      <Card.Text>C</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  <Card
-                    className={`${styles.stackCard} mx-md-2 my-sm-3`}
-                    bg="info"
-                    text="dark"
-                    border="success"
-                  >
-                    <Card.Img
-                      variant="top"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg"
-                    />
-                    <Card.Body>
-                      <Card.Text>HTML</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  <Card
-                    className={`${styles.stackCard} mx-md-2 my-sm-3`}
-                    bg="info"
-                    text="dark"
-                    border="success"
-                  >
-                    <Card.Img
-                      variant="top"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg"
-                    />
-                    <Card.Body>
-                      <Card.Text>SQL</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  <Card
-                    className={`${styles.stackCard} mx-md-2 my-sm-3`}
-                    bg="info"
-                    text="dark"
-                    border="success"
-                  >
-                    <Card.Img
-                      variant="top"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-plain.svg"
-                    />
-                    <Card.Body>
-                      <Card.Text>Linux</Card.Text>
-                    </Card.Body>
-                  </Card>
-                  {/* </Row> */}
-                </CardGroup>
+                <TechStack />
               </section>
             </div>
           </main>
